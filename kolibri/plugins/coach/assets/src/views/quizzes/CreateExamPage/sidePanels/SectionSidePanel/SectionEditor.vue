3d43fc9caa4ae315b86234bc9117bfc7b950e2a8
Allan Otodi Opeto <103313919+AllanOXDi@users.noreply.github.com>
2025-05-30 19:51:43 +0300

    use  KDS dropdshadows

--- kolibri/plugins/coach/assets/src/views/quizzes/CreateExamPage/sidePanels/SectionSidePanel/SectionEditor.vue
+++ kolibri/plugins/coach/assets/src/views/quizzes/CreateExamPage/sidePanels/SectionSidePanel/SectionEditor.vue
@@ -465,9 +465,7 @@
     padding: 1em 2em;
     margin-top: 1em;
     background-color: #ffffff;
-    box-shadow:
-      0 0 2px rgba(0, 0, 0, 0.9),
-      0 2px 2px rgba(0, 0, 0, 0.15);
+    @extend %dropshadow-1dp;
   }
 
   /deep/ .textbox {
