d72076faf514ef844398e9f32c32ebe232028ce9
Alex Velez <velezllaquealex@gmail.com>
2025-06-05 08:41:56 -0500

    Implement keyboard focus navigation

--- kolibri/plugins/facility/assets/src/views/ManageClassPage/SelectableList.vue
+++ kolibri/plugins/facility/assets/src/views/ManageClassPage/SelectableList.vue
@@ -10,10 +10,18 @@
       class="list-options"
       tabindex="0"
       role="listbox"
+      data-focus="true"
+      aria-multiselectable="true"
+      :style="{ outline: 'none' }"
       :aria-labelledby="ariaLabelledby"
+      :aria-activedescendant="getElementOptionId(focusedOption)"
+      @focus="onListFocus"
+      @blur="onListBlur"
+      @keydown="handleKeydown"
     >
       <li
         v-for="option in options"
+        :id="getElementOptionId(option)"
         :key="option.id"
         role="option"
         class="list-option"
@@ -25,12 +33,11 @@
             ':not(:last-child)': {
               borderBottom: '1px solid ' + $themeTokens.fineLine,
             },
+            ...(isOptionFocused(option) ? $coreOutline : {}),
           })
         "
         :aria-selected="isOptionSelected(option)"
         @click="toggleOption(option)"
-        @keydown.enter="toggleOption(option)"
-        @keydown.space="toggleOption(option)"
       >
         <KCheckbox
           unfocusable
@@ -46,13 +53,24 @@
 
 <script>
 
-  import { computed, toRefs } from 'vue';
+  import { ref, computed, toRefs, getCurrentInstance } from 'vue';
   import { validateObject } from 'kolibri/utils/objectSpecs';
 
   export default {
     name: 'SelectableList',
     setup(props, { emit }) {
-      const { value } = toRefs(props);
+      const { value, options } = toRefs(props);
+      const focusedIndex = ref(null);
+
+      const focusedOption = computed(() => {
+        if (focusedIndex.value === null || !options.value.length) {
+          return null;
+        }
+        return options.value[focusedIndex.value];
+      });
+
+      const instance = getCurrentInstance();
+      const uid = instance.proxy._uid;
 
       const selectedOptions = computed({
         get() {
@@ -67,7 +85,15 @@
         return selectedOptions.value.includes(option.id);
       }
 
+      function isOptionFocused(option) {
+        return focusedOption.value?.id === option.id;
+      }
+
       function toggleOption(option) {
+        if (!option) {
+          return;
+        }
+
         if (isOptionSelected(option)) {
           selectedOptions.value = value.value.filter(id => id !== option.id);
         } else {
@@ -75,9 +101,71 @@
         }
       }
 
+      function getElementOptionId(option) {
+        if (!option?.id) {
+          return null;
+        }
+
+        return `sl-option-${uid}-${option.id}`;
+      }
+
+      function onListFocus() {
+        if (!options.value.length) {
+          return;
+        }
+        focusedIndex.value = 0;
+      }
+
+      function onListBlur() {
+        focusedIndex.value = null;
+      }
+
+      function handleFocusNavigation(key) {
+        const diff = key === 'ArrowDown' ? 1 : -1;
+        // adding options.length and using modulo to wrap around
+        // enables circular navigation
+        focusedIndex.value =
+          (focusedIndex.value + diff + options.value.length) % options.value.length;
+      }
+
+      function handleKeydown(event) {
+        if (!options.value.length) {
+          return;
+        }
+
+        const { key } = event;
+
+        switch (key) {
+          case 'ArrowDown':
+          case 'ArrowUp':
+            handleFocusNavigation(key);
+            break;
+          case 'Home':
+            focusedIndex.value = 0;
+            break;
+          case 'End':
+            focusedIndex.value = options.value.length - 1;
+            break;
+          case ' ':
+            toggleOption(focusedOption.value);
+            break;
+          default:
+            // Early return for unsupported keys so that we don't prevent default behavior
+            return;
+        }
+
+        event.preventDefault();
+      }
+
       return {
+        onListBlur,
+        onListFocus,
         toggleOption,
+        focusedOption,
+        isOptionFocused,
         isOptionSelected,
+        getElementOptionId,
+        handleKeydown,
       };
     },
     props: {
