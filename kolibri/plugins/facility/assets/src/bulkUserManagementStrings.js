18e9c7ebdd1c4dfcf2d80e8bc02f6ab460002958
Jacob Pierce <w.jacobpierce@gmail.com>
2025-06-09 11:18:34 -0700

    apply proper ICU syntax for plurals

--- kolibri/plugins/facility/assets/src/bulkUserManagementStrings.js
+++ kolibri/plugins/facility/assets/src/bulkUserManagementStrings.js
@@ -12,11 +12,11 @@ export const bulkUserManagementStrings = createTranslator('BulkUserManagementStr
     context: "Placeholder text for user search input",
   },
   numUsersSelected: {
-    message: "{n, number} users selected",
+    message: "{n, number} {n, plural, one {user} other {users}} selected",
     context: "A label showing the number of users selected",
   },
   numFilters: {
-    message: "{n, number} filters",
+    message: "{n, number} {n, plural, one {filter} other {filters}}",
     context: "A label showing the number of filters active",
   },
 
@@ -52,21 +52,21 @@ export const bulkUserManagementStrings = createTranslator('BulkUserManagementStr
 
   // Selection warnings
   numUsersNotEnrolled: {
-    message: "{num, number} users are not enrolled in any class",
+    message: "{num, number} {num, plural, one {user is} other {users are}} are not enrolled in any class",
     context: "A notice indicating the number of users that are selected which are not enrolled in a class"
   },
   numUsersCoaches: {
-    message: "{num, number} users are coaches",
+    message: "{num, number} {num, plural, one {user is} other {users are}} are coaches",
     context: "A notice indicating the number of users the user selected that are coaches"
   },
 
   // Assign coaches to class
   undoAssignCoachHeading: {
-    message: "{num, number} users have been removed. Undo this?",
+    message: "{num, number} {num, plural, one {coach has} other {coaches have}} been removed. Undo this?",
     context: "Confirmation heading that allows user to undo their action of removing users"
   },
   undoAssignCoachMessage: {
-    message: "You've successfully assigned {numUsers, number} users from {numClasses, number} classes. If this was a mistake, you can undo it.",
+    message: "You've successfully assigned {numUsers, number} {numUsers, plural, one {user} other {users}} from {numClasses, number} {numClasses, plural, one {class} other {classes}}. If this was a mistake, you can undo it.",
     context: "Snackbar notification message indicating success"
   },
   coachesAssignedNotice: {
@@ -84,11 +84,11 @@ export const bulkUserManagementStrings = createTranslator('BulkUserManagementStr
     context: "Warning message about users already not in selected classes"
   },
   undoUsersRemovedHeading: {
-    message: "{num, number} users have been removed. Undo this?",
+    message: "{num, number} {num, plural, one {user has} other {users have}} been removed. Undo this?",
     context: "Heading for undo confirmation after removing users"
   },
   undoUsersRemovedMessage: {
-    message: "You've successfully removed {numUsers, number} users from {numClasses, number} classes. If this was a mistake, you can undo it.",
+    message: "You've successfully removed {numUsers, number} {numUsers, plural, one {user} other {users}} from {numClasses, number} {numClasses, plural, one {class} other {classes}}. If this was a mistake, you can undo it.",
     context: "Detailed message for undo confirmation after removing users"
   },
   usersRemovedNotice: {
@@ -106,11 +106,11 @@ export const bulkUserManagementStrings = createTranslator('BulkUserManagementStr
     context: "Warning message about users already in selected classes"
   },
   undoUsersEnrolledHeading: {
-    message: "{num, number} users have been enrolled. Undo this?",
+    message: "{num, number} {num, plural, one {user} other {users}} have been enrolled. Undo this?",
     context: "Heading for undo confirmation after enrolling users"
   },
   undoUsersEnrolledMessage: {
-    message: "You've successfully enrolled {numUsers, number} users from {numClasses, number} classes. If this was a mistake, you can undo it.",
+    message: "You've successfully enrolled {numUsers, number} {numUsers, plural, one {user} other {users}} to {numClasses, number} {numClasses, plural, one {class} other {classes}}. If this was a mistake, you can undo it.",
     context: "Detailed message for undo confirmation after enrolling users"
   },
   usersEnrolledNotice: {
@@ -124,11 +124,11 @@ export const bulkUserManagementStrings = createTranslator('BulkUserManagementStr
 
   // Move to trash
   undoTrashHeading: {
-    message: "{num, number} users have been moved to trash. Undo this?",
+    message: "{num, number} {num, plural, one {user has} other {users have}} been moved to trash. Undo this?",
     context: "Confirmation heading asking if user wants to undo moving multiple users to trash"
   },
   undoTrashMessageA: {
-    message: "You've successfully moved {numUsers, number} users to trash.",
+    message: "You've successfully moved {numUsers, number} {numUsers, plural, one {user} other {users}} to trash.",
     context: "Success notification after users have been moved to trash"
   },
   undoTrashMessageB: {
