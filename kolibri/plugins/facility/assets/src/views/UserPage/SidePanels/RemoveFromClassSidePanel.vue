9444a428c9c9d2bf9689a2ee81b66649e3268899
Liana Harris <46411498+LianaHarris360@users.noreply.github.com>
2025-06-26 12:06:57 -0500

    Add pageName constants and placeholder components for side panel bulk user actions

---
+++ kolibri/plugins/facility/assets/src/views/UserPage/SidePanels/RemoveFromClassSidePanel.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <SidePanelModal
+    alignment="right"
+    sidePanelWidth="700px"
+    @closePanel="$router.back()"
+  >
+    <template #header>
+      <h1>{{ coreString('classesLabel') }}</h1>
+    </template>
+  </SidePanelModal>
+
+</template>
+
+
+<script>
+
+  import SidePanelModal from 'kolibri-common/components/SidePanelModal';
+  import commonCoreStrings from 'kolibri/uiText/commonCoreStrings';
+
+  export default {
+    name: 'RemoveFromClassSidePanel',
+    components: {
+      SidePanelModal,
+    },
+    mixins: [commonCoreStrings],
+  };
+
+</script>
