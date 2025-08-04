94dfbf2392639bbd4a47612cdf44873f29f673c7
Allan Otodi Opeto <103313919+AllanOXDi@users.noreply.github.com>
2025-08-04 22:14:03 +0300

    reverts changes

--- kolibri/plugins/facility/assets/src/views/users/sidePanels/UserCreate/ClassesSelect.vue
+++ kolibri/plugins/facility/assets/src/views/users/sidePanels/UserCreate/ClassesSelect.vue
@@ -24,6 +24,7 @@
 
   import { computed, toRefs } from 'vue';
   import { bulkUserManagementStrings } from 'kolibri-common/strings/bulkUserManagementStrings';
+
   import { ClassesActions } from '../../../../constants';
 
   const ALL_VALUE = 'ALL';
