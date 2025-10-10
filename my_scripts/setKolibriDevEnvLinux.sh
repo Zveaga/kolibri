10de40d6a3d24192e2cddbb4ebe2a13c3c66f1d9
rares <rares.rares.cox@gmail.com>
2025-10-10 10:40:23 -0400

    created script for setting up the development environment

---
+++ my_scripts/setKolibriDevEnvLinux.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e  # stop if any command fails
+
+green_echo() {
+  echo -e "${GREEN}$1${NC}"
+}
+
+green_echo "🔹 Changing directory to project root..."
+cd ../
+
+green_echo "🔹 Initialize Git Large File Storage..."
+git lfs install
+
+green_echo "🔹 Creating Virtual Environment..."
+python3 -m venv venv
+
+green_echo "🔹 Activating Virtual Environment..."
+source venv/bin/activate
+
+green_echo "🔹 Installing Python dependencies..."
+pip install --upgrade -r requirements.txt
+pip install --upgrade -r requirements/dev.txt
+pip install -e .
+
+green_echo "🔹 Installing node..."
+nodeenv -p --node=20.19.3
+
+green_echo "🔹 Installing yarn..."
+npm install -g yarn
+
+green_echo "🔹 Installing other project dependencies..."
+yarn install
+
+green_echo "🔹 Initialize Kolibri database..."
+kolibri manage migrate
+
+green_echo "✅ Done!"
+green_echo "You can start the server by running: yarn run devserver"
