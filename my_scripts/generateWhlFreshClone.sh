0936e22d88cd405ed016db43482692977d835b82
rares <rares.rares.cox@gmail.com>
2025-10-10 11:06:36 -0400

    updated scripts

--- my_scripts/generateWhlFreshClone.sh
+++ my_scripts/generateWhlFreshClone.sh
@@ -10,11 +10,26 @@ green_echo() {
   echo -e "${GREEN}$1${NC}"
 }
 
+green_echo "🔹 Creating Virtual Environment..."
+python3 -m venv venv
+
 green_echo "🔹 Activating Virtual Environment..."
 source venv/bin/activate
 
 green_echo "🔹 Installing Python dependencies..."
+pip install --upgrade -r requirements.txt
+pip install --upgrade -r requirements/dev.txt
+pip install -e .
 pip install beautifulsoup4
+
+green_echo "🔹 Installing node..."
+nodeenv -p --node=20.19.3
+
+green_echo "🔹 Installing yarn..."
+npm install -g yarn
+
+green_echo "🔹 Installing other project dependencies..."
+yarn install
 
 green_echo "🔹 Increase Node heap memory limit to 4GB..."
 if ! grep -q 'NODE_OPTIONS="--max-old-space-size=4096"' Makefile; then
