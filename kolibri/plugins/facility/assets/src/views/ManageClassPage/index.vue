0039873e199f4c67edefca8bb7e190d4528ab217
rares <rares.rares.cox@gmail.com>
2025-10-14 03:56:39 -0400

    reverted the UI change in ManageClassPage/index.vue

--- kolibri/plugins/facility/assets/src/views/ManageClassPage/index.vue
+++ kolibri/plugins/facility/assets/src/views/ManageClassPage/index.vue
@@ -32,19 +32,6 @@
             class="move-down"
             @click="displayModal(Modals.CREATE_CLASS)"
           />
-		  
-		  <KButton
-            :text="$tr('addNew')"
-            :primary="true"
-            class="move-down"
-            @click="displayModal(Modals.CREATE_CLASS)"
-          />
-		   <KButton
-            :text="$tr('addNew')"
-            :primary="true"
-            class="move-down"
-            @click="displayModal(Modals.CREATE_CLASS)"
-          />
         </KGridItem>
       </KGrid>
       <KTable
